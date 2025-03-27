aaefc5e6271b7c30ecfe02600b5edaff38dde9a5
ibraaheem314 <ibrahima.diallo1249852@gmail.com>
2025-03-27 10:06:39 +0100

    Training EMA with 20 epochs

--- train.py
+++ train.py
@@ -5,9 +5,15 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 import os
+import copy
 from model import UNet
 from diffusion import Diffusion
 from tqdm import tqdm
+
+@torch.no_grad()
+def update_ema(ema_model, model, decay=0.999):
+    for ema_param, param in zip(ema_model.parameters(), model.parameters()):
+        ema_param.data.mul_(decay).add_(param.data, alpha=1 - decay)
 
 def main():
     # Configurations
@@ -16,7 +22,7 @@ def main():
     if DEVICE == 'cuda':
         print('GPU Name:', torch.cuda.get_device_name(0))
     BATCH_SIZE = 64
-    EPOCHS = 100
+    EPOCHS = 20
     LR = 2e-4
 
     # Data Loading
@@ -30,6 +36,8 @@ def main():
 
     # Model and Diffusion
     model = UNet().to(DEVICE)
+    ema_model = copy.deepcopy(model)
+    ema_model.eval()
     diffusion = Diffusion(device=DEVICE)
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
@@ -49,12 +57,29 @@ def main():
             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             optimizer.step()
 
+            update_ema(ema_model, model)
+
             global_step += 1
             pbar.set_description(f"Epoch [{epoch+1}/{EPOCHS}] Loss: {loss.item():.4f}")
 
         # Save checkpoint at the end of the last epoch
         if epoch + 1 == EPOCHS:
             torch.save(model.state_dict(), f'model_epoch_{epoch+1}.pth')
+            torch.save(ema_model.state_dict(), f'model_ema_epoch_{epoch+1}.pth')
+            
+        # Comparaison finale entre le modèle entraîné et l'EMA
+        
+    total_diff = 0.
+    total_norm = 0.
+    for param, ema_param in zip(model.parameters(), ema_model.parameters()):
+        diff = (param.data - ema_param.data).abs().mean().item()
+        total_diff += diff
+        total_norm += param.data.abs().mean().item()
+
+    print(f"\n📊 Moyenne des écarts absolus entre model et ema_model : {total_diff:.6f}")
+    print(f"📊 Norme moyenne des poids du model : {total_norm:.6f}")
+    print(f"📉 Ratio de différence relative : {(total_diff / total_norm):.4%}")
+
 
 if __name__ == '__main__':
     main()
